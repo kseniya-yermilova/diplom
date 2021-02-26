d990e8183f6d0b4c9ccde9bb04e05ff37961b520
kseniya <ksyuw@yahoo.com>
2021-02-26 16:48:23 +0200

    Data section test

--- src/main/java/net/hotel/pages/RegistrationHotelPage.java
+++ src/main/java/net/hotel/pages/RegistrationHotelPage.java
@@ -4,12 +4,14 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class RegistrationHotelPage {
     private WebDriver driver;
 
     public RegistrationHotelPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
     public boolean isPageLoad(){
         JavascriptExecutor js = (JavascriptExecutor) driver;
@@ -18,4 +20,15 @@ public class RegistrationHotelPage {
 
     @FindBy(xpath = "//form[@id='add_hotel']")
     private WebElement dataSection;
+
+    @FindBy(xpath = "//span[text()='Save']")
+    private WebElement saveButton;
+
+    public boolean isDataSectionDisplayed(){
+        return dataSection.isDisplayed();
+    }
+
+    public boolean isSaveButtonDisplayed(){
+        return saveButton.isDisplayed();
+    }
 }
--- src/test/java/net/hotel/test/RegistrationHotelPageTest.java
+++ src/test/java/net/hotel/test/RegistrationHotelPageTest.java
@@ -10,4 +10,14 @@ public class RegistrationHotelPageTest extends BaseTest{
         boolean result = new MainPage(driver).openRegistrationHotelPage().isPageLoad();
         Assert.assertTrue(result,"Page should be loaded");
     }
+    @Test
+    public void testDataSectionIsDisplayed(){
+        boolean result = new MainPage(driver).openRegistrationHotelPage().isDataSectionDisplayed();
+        Assert.assertTrue(result,"Data section should be displayed");
+    }
+    @Test
+    public void testSaveButtonIsDisplayed(){
+        boolean result = new MainPage(driver).openRegistrationHotelPage().isSaveButtonDisplayed();
+        Assert.assertTrue(result,"Save button should be displayed");
+    }
 }
